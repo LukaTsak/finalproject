cbc21991ae6d9e532f11fe608e20e9e9bf389511
LukaTsak <lukatsakadze10@gmail.com>
2025-04-29 19:08:00 +0400

    2

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardComponent } from "../card/card.component";
 import { ApiService } from '../services/api.service';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-main',
-  imports: [CardComponent, RouterModule],
+  standalone: true,
+  imports: [CardComponent, RouterModule, CommonModule, FormsModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
-  constructor(private httpData : ApiService){}
+export class MainComponent implements OnInit {
+  title = 'finalproject';
+  data: any[] = [];
+  searchTerm: string = '';
 
+  constructor(private apiService: ApiService, private http: HttpClient) { }
 
-  ngOnInit(){
-    this.httpData.getData().subscribe((res: any) => {
-      console.log(res.data)
-    })
+  ngOnInit() {
+    console.log('hi')
+    this.apiService.getData().subscribe((response: any) => {
+      console.log(response);
+      console.log('hi')
+    });
   }
 }
